cadeafc4d9909939780fcdd51007aff3684d0ec2
Aaron W. Tabor <aarontabor50@gmail.com>
2015-05-03 16:07:51 -0300

    Initial working code.
    Two separate tools:
      keyboardLight - controls the keyboard backlight intensity
      displayLight - control the monitor intensity

---
+++ brightnessLib.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int getBrightnessPercentage(char *deviceDirectory) {
+  char currentBrightnessFilename[128], maxBrightnessFilename[128];
+  FILE *currentBrightnessFile, *maxBrightnessFile;
+  int currentBrightness, maxBrightness, currentPercentage;
+
+  sprintf(currentBrightnessFilename, "%s/%s", deviceDirectory, "brightness");
+  sprintf(maxBrightnessFilename, "%s/%s", deviceDirectory, "max_brightness");
+
+  currentBrightnessFile = fopen(currentBrightnessFilename, "r");
+  if (currentBrightnessFile == 0) {
+    printf("Error opening file: %s\n", currentBrightnessFilename);
+    exit(1);
+  }
+
+  maxBrightnessFile = fopen(maxBrightnessFilename, "r");
+  if (maxBrightnessFile == 0) {
+    printf("Error opening file: %s\n", maxBrightnessFilename);
+    exit(2);
+  }
+
+  fscanf(currentBrightnessFile, "%d", &currentBrightness);
+  fscanf(maxBrightnessFile, "%d", &maxBrightness);
+  fclose(currentBrightnessFile);
+  fclose(maxBrightnessFile);
+
+  currentPercentage = (currentBrightness*100) / maxBrightness;
+  printf("[INFO] current brightness percentage: %d\n", currentPercentage);
+  return currentPercentage;
+}
+
+int setBrightnessPercentage(char *deviceDirectory, int percentage) {
+  char currentBrightnessFilename[128], maxBrightnessFilename[128];
+  FILE *currentBrightnessFile, *maxBrightnessFile;
+  int maxBrightness, newBrightness;
+
+  sprintf(currentBrightnessFilename, "%s/%s", deviceDirectory, "brightness");
+  sprintf(maxBrightnessFilename, "%s/%s", deviceDirectory, "max_brightness");
+
+  currentBrightnessFile = fopen(currentBrightnessFilename, "w");
+  if (currentBrightnessFile == 0) {
+    printf("Error opening file: %s\n", currentBrightnessFilename);
+    exit(1);
+  }
+
+  maxBrightnessFile = fopen(maxBrightnessFilename, "r");
+  if (maxBrightnessFile == 0) {
+    printf("Error opening file: %s\n", maxBrightnessFilename);
+    exit(2);
+  }
+
+  fscanf(maxBrightnessFile, "%d", &maxBrightness);
+  fclose(maxBrightnessFile);
+
+  newBrightness = (maxBrightness*percentage) / 100;
+  printf("[INFO] writing brightness value: %d\n", newBrightness);
+  fprintf(currentBrightnessFile, "%d", newBrightness);
+  fclose(currentBrightnessFile);
+  return 0;
+}
---
+++ keyboardLight.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+
+#define brightnessDirectory "/sys/class/leds/smc::kbd_backlight"
+#define INCREMENT_VALUE 10
+
+
+int doIncrement(int currentBrightnessPercentage);
+int doDecrement(int currentBrightnessPercentage);
+int setBrightnessPercentage(char *directory, int percentage);
+
+int main(int argc, char *argv[]) {
+  int brightnessPercentage = getBrightnessPercentage(brightnessDirectory);
+
+  char *option = argv[1];
+  if (strcmp(option, "--increment") == 0) {
+  printf("before\n");
+    doIncrement(brightnessPercentage);
+  printf("after\n");
+  } else if (strcmp(option, "--decrement") == 0) {
+    doDecrement(brightnessPercentage);
+  } else if (strcmp(option, "--set") == 0) {
+    int newPercentage = atoi(argv[2]);
+    setBrightnessPercentage(brightnessDirectory, newPercentage);
+  } else {
+    printf("Uasge: keyboardLight <option> [brightness-percentage]\n");
+  }
+
+  return 0;
+}
+
+int doIncrement(int currentBrightnessPercentage) {
+  int newPercentage = currentBrightnessPercentage + INCREMENT_VALUE;
+  setBrightnessPercentage(brightnessDirectory, (newPercentage > 100) ? 100 : newPercentage);
+  return 0;
+}
+
+int doDecrement(int currentBrightnessPercentage) {
+  int newPercentage = currentBrightnessPercentage - INCREMENT_VALUE;
+  setBrightnessPercentage(brightnessDirectory, (newPercentage < 0) ? 0 : newPercentage);
+  return 0;
+}
