a7e5620a1388f1699e7468c62e2fb45cbe4ad0c2
Aaron W. Tabor <aarontabor50@gmail.com>
2015-05-04 19:21:04 -0300

    extracted file read/write into local function.

--- brightnessLib.c
+++ brightnessLib.c
@@ -3,30 +3,18 @@
 #include "brightnessLib.h"
 
 
+int readInteger(char *filename);
+void writeInteger(char *filename, int value);
+
 int getBrightnessPercentage(char *deviceDirectory) {
   char currentBrightnessFilename[128], maxBrightnessFilename[128];
-  FILE *currentBrightnessFile, *maxBrightnessFile;
   int currentBrightness, maxBrightness, currentPercentage;
 
   sprintf(currentBrightnessFilename, "%s/%s", deviceDirectory, "brightness");
   sprintf(maxBrightnessFilename, "%s/%s", deviceDirectory, "max_brightness");
 
-  currentBrightnessFile = fopen(currentBrightnessFilename, "r");
-  if (currentBrightnessFile == 0) {
-    printf("Error opening file: %s\n", currentBrightnessFilename);
-    exit(1);
-  }
-
-  maxBrightnessFile = fopen(maxBrightnessFilename, "r");
-  if (maxBrightnessFile == 0) {
-    printf("Error opening file: %s\n", maxBrightnessFilename);
-    exit(2);
-  }
-
-  fscanf(currentBrightnessFile, "%d", &currentBrightness);
-  fscanf(maxBrightnessFile, "%d", &maxBrightness);
-  fclose(currentBrightnessFile);
-  fclose(maxBrightnessFile);
+  currentBrightness = readInteger(currentBrightnessFilename);
+  maxBrightness = readInteger(maxBrightnessFilename);
 
   currentPercentage = (currentBrightness*100) / maxBrightness;
   printf("[INFO] current brightness percentage: %d\n", currentPercentage);
@@ -35,30 +23,41 @@ int getBrightnessPercentage(char *deviceDirectory) {
 
 int setBrightnessPercentage(char *deviceDirectory, int percentage) {
   char currentBrightnessFilename[128], maxBrightnessFilename[128];
-  FILE *currentBrightnessFile, *maxBrightnessFile;
   int maxBrightness, newBrightness;
 
   sprintf(currentBrightnessFilename, "%s/%s", deviceDirectory, "brightness");
   sprintf(maxBrightnessFilename, "%s/%s", deviceDirectory, "max_brightness");
 
-  currentBrightnessFile = fopen(currentBrightnessFilename, "w");
-  if (currentBrightnessFile == 0) {
-    printf("Error opening file: %s\n", currentBrightnessFilename);
-    exit(1);
-  }
-
-  maxBrightnessFile = fopen(maxBrightnessFilename, "r");
-  if (maxBrightnessFile == 0) {
-    printf("Error opening file: %s\n", maxBrightnessFilename);
-    exit(2);
-  }
-
-  fscanf(maxBrightnessFile, "%d", &maxBrightness);
-  fclose(maxBrightnessFile);
+  maxBrightness = readInteger(maxBrightnessFilename);
 
   newBrightness = (maxBrightness*percentage) / 100;
   printf("[INFO] writing brightness value: %d\n", newBrightness);
-  fprintf(currentBrightnessFile, "%d", newBrightness);
-  fclose(currentBrightnessFile);
+  writeInteger(currentBrightnessFilename, newBrightness);
   return 0;
+}
+
+int readInteger(char *filename) {
+  FILE *file;
+  int value;
+
+  file = fopen(filename, "r");
+  if (file == 0) {
+    printf("Error opening file: %s\n", filename);
+    exit(1);
+  }
+  fscanf(file, "%d", &value);
+  fclose(file);
+  return value;
+}
+
+void writeInteger(char *filename, int value) {
+  FILE *file;
+
+  file = fopen(filename, "w");
+  if (file == 0) {
+    printf("Error opening file: %s\n", filename);
+    exit(1);
+  }
+  fprintf(file, "%d", value);
+  fclose(file);
 }
